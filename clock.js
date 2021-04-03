b77864e69de1016c3716b6238a274043f9e94e01
Junho-Cha <79741497+Junho-Cha@users.noreply.github.com>
2021-04-03 22:38:34 +0900

    채팅창 날짜표시
    날짜표시 js 추가

--- clock.js
+++ clock.js
@@ -1,5 +1,6 @@
 const clockContainer = document.querySelector(".rt-clock");
 const clockTitle = clockContainer.querySelector("#rt-clock");
+const timestamp = document.querySelector(".chat-timestamp");
 
 function getTime(){
     const date = new Date();
@@ -7,6 +8,16 @@ function getTime(){
     const hours = date.getHours();
     clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${
         minutes < 10 ? `0${minutes}` : minutes}`;
+
+    const year = date.getFullYear();
+    const month = date.getMonth()+1;
+    const dates = date.getDate();
+
+    const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    const today = new Date().getDay();
+    const dayOfWeek = week[today];
+
+    timestamp.innerText = `${year}년 ${month}월 ${dates}일 ${dayOfWeek}`
 }
 
 function init(){
